cc625843d422d4608654ee3453de98cdcbf7f1ad
tbatenorio <136942374+tbatenorio@users.noreply.github.com>
2023-07-20 16:34:47 -0300

    Update Exploratory_Analysis.py

--- Pages/Exploratory_Analysis.py
+++ Pages/Exploratory_Analysis.py
@@ -16,6 +16,24 @@ bSidebar = st.sidebar
 bBox1 = st.container()
 bBox2 = st.container()
 
+insith_01 = """
+**The 'Commercial', 'Research', 'Development', 'Talent' indicators are highly correlated, which 
+gives us the information that the private sector is the great driving force of this segment, 
+with its products and services subsequently absorbed by the entire chain of technological services.**
+"""
+
+insith_02 = """
+**Countries named as 'Developed' are top ranked in the AI ​​sector. 
+Precisely for this reason, they lead the growth of the world with innovation 
+and quality of life for the entire population and confirm themselves as the 
+most powerful nations on earth.**
+"""
+
+insith_03 = """
+**The 'Liberal Democracy' system of government is prevalent among nations that invest in artificial 
+intelligence technology. This confirms his thesis that people and the market should be free to develop 
+and generate knowledge and wealth without government intervention.**
+"""
 
 with bHeader:
     # elemento de titulo
@@ -31,6 +49,7 @@ with bSidebar:
     st.image(Image.open(f"Assets/{option_indicator}.jpg"), caption= f"{option_indicator} Indicator")
     
 with bBox1:
+    
     # Calcular a matriz de correlação entre os Indicadores
     indicators = dataset.copy()
     indicators.drop(['Total score','Country','Region','Cluster','Income group','Political regime'], axis=1, inplace=True)
@@ -40,9 +59,13 @@ with bBox1:
     fig = px.imshow(correlation_matrix, text_auto=True, color_continuous_scale='Blues', title='Correlation about the Diferents Indicators')
     fig.update_layout(width=700, height=700)
     st.plotly_chart(fig, use_container_width=True) 
- 
+
+    
+    #Insight
+    st.markdown(insith_01)
 
 with bBox2: 
+    
     #Graph 1
     st.header(f"TOP 10 Countries for: {option_indicator}")
     
@@ -54,6 +77,10 @@ with bBox2:
     fig1 = px.bar(dff, x= dff[option_indicator], y= dff.Country, orientation='h')
     st.plotly_chart(fig1, use_container_width=True) 
 
+    
+    st.markdown(insith_02)
+
+  
     #Graph 2
     st.subheader('Political Regime Distribution')
     regimes = dff['Political regime'].value_counts(normalize = True)
@@ -67,6 +94,9 @@ with bBox2:
 
     fig2 = go.Figure(data=[go.Pie(labels= regimes.index, values= regimes.values, pull= pull_values)])
     st.plotly_chart(fig2, use_container_width=True)
+
+    
+    st.markdown(insith_03)
 
 
 
